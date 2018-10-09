25fcca63caa29c3787be065769dd66f83cc09af5
soizic Lambert <soizic.lee@gmail.com>
2018-10-09 11:30:38 +0200

    ajout fichier accueil doc en cours

---
+++ src/app/accueil-doc/accueil-doc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accueil-doc',
+  templateUrl: './accueil-doc.component.html',
+  styleUrls: ['./accueil-doc.component.css']
+})
+export class AccueilDocComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { PageMenuQuizComponent } from './page-menu-quiz/page-menu-quiz.component';
+import { AccueilDocComponent } from './accueil-doc/accueil-doc.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PageMenuQuizComponent
+    PageMenuQuizComponent,
+    AccueilDocComponent
   ],
   imports: [
     BrowserModule
